0e2f861063bbac9ed989b896dea0bb119c3c0f87
Ryan Duff <ryanduff613@gmail.com>
2020-04-16 12:05:08 -0700

    meaningful fail - test to determine if code creates  a player is working. test fails because Player class has not been created

--- _tests_/RPG.test.js
+++ _tests_/RPG.test.js
@@ -1,1 +1,12 @@
 import { Player } from "./../src/businessLogic.js";
+
+describe('Player', () => {
+
+  test('should create a new instance of Player', () => {
+    var player = new Player('andy');
+    expect(player.name).toEqual('andy');
+    expect(player.health).toEqual(50);
+    expect(player.strenght).toEqual(50);
+  });
+
+});
